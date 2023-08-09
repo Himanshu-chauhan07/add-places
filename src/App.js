38b2366b2e8d01efb88aee69b26cb84118bcf30a
himanshu-chauhan07 <iamhimanshuc@gmail.com>
2023-08-09 11:55:27 +0530

    react router dom setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import React from 'react';
+
+import {BrowserRouter as Router,Route,Redirect,Switch} from 'react-router-dom';
+import User from './User/Pages/User';
+import NewPlaces from './Places/Pages/NewPlace';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-     hello world
-    </div>
+    <Router>
+     <Switch>
+     <Route path="/" exact>
+      <User />
+     </Route>
+     <Route path="/places/new">
+      <NewPlaces/>
+     </Route>
+     <Route>
+      <Redirect to="/" />
+     </Route>
+     </Switch>
+    </Router>
   );
 }
 
---
+++ src/Places/Pages/NewPlace.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './NewPlace.css'
+
+const NewPlaces = () => {
+    return (
+        <div className="NewPlaces">
+            <h1>New Places</h1>
+        </div>
+    )
+}
+
+export default NewPlaces;
