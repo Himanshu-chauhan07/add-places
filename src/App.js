311bc138723ee1a965b54f2dd047cdfb86ebcfa3
Himanshu-chauhan07 <iamhimanshuc@gmail.com>
2023-08-23 11:18:53 +0530

    nav links added

--- src/App.js
+++ src/App.js
@@ -3,23 +3,27 @@ import React from 'react';
 import {BrowserRouter as Router,Route,Redirect,Switch} from 'react-router-dom';
 import User from './User/Pages/User';
 import NewPlaces from './Places/Pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 import './App.css';
 
 
-function App() {
+const App = () => {
   return (
     <Router>
+      <MainNavigation/>
+      <main>
      <Switch>
-     <Route path="/" exact>
+     <Route path="/" exact> 
       <User />
      </Route>
-     <Route path="/places/new">
+     <Route path="/places/new" exact>
       <NewPlaces/>
      </Route>
      <Route>
       <Redirect to="/" />
      </Route>
      </Switch>
+     </main>
     </Router>
   );
 }
--- src/User/Pages/User.js
+++ src/User/Pages/User.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React from "react";
+
+import UsersList from "../components/UsersList";
 
 const User = () => {
-    return(
-        <div>
-            <h1>User hello</h1>
-        </div>
-    )
-}
+  const USERS = [
+    {
+      id: "u1",
+      name: "Himanshu Chauhan",
+      image:
+        "https://i0.wp.com/www.pixelimage.tv/CONTENT_wp-content/uploads/2018/09/IMG_3133.jpg?fit=4032%2C3024",
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
+};
 
 export default User;
-
